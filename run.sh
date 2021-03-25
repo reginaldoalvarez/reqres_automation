84b0b51d76095b219349071a0b6fc0d5fa94acfd
Julian Velez <45567796+boya678@users.noreply.github.com>
2021-03-25 10:27:39 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 gradle clean build test -PENVURL='https://reqres.in'
 if [ $? -eq 0 ]; then
-    curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"*Successful* Reqres api Automated Regression Test :+1:"}}]}' https://hooks.slack.com/services/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
+    curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"*Successful* Reqres api Automated Regression Test :+1:"}}]}' https://hooks.slack.com/services/3453JJ/B0234234M6RU67/3uxQr0qRdP7WciQWOW2Y07HG
 else
-    curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"*´Failed´* Reqres api Automated Regression Test :-1:"}}]}' https://hooks.slack.com/services/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
+    curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"*´Failed´* Reqres api Automated Regression Test :-1:"}}]}' https://hooks.slack.com/services/3453JJ/B0234234M6RU67/3uxQr0qRdP7WciQWOW2Y07HG
 fi
