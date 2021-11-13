837f06b37206f13d48cd75c65469f204b1b43c1d
Julian Velez <julian.velez@t-evolvers.com>
2021-11-13 10:39:30 -0500

    version para capacitación

--- src/test/java/com/appgate/regres/stepdefinitions/ParameterDefinitions.java
+++ src/test/java/com/appgate/regres/stepdefinitions/ParameterDefinitions.java
@@ -1,38 +1,19 @@
 package com.appgate.regres.stepdefinitions;
 
-import java.util.Map;
 
-import com.appgate.regres.models.User;
-import com.appgate.regres.models.UserCreateRequest;
-import com.appgate.regres.models.UserUpdateRequest;
-
-import io.cucumber.java.DataTableType;
 import io.cucumber.java.ParameterType;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actors.OnStage;
 
+import java.util.Map;
+
 public class ParameterDefinitions {
+
 
 	@ParameterType(".*")
 	public Actor actor(String actorName) {
 		return OnStage.theActorCalled(actorName);
 	}
 
-	@DataTableType
-	public User user(Map<String, String> user) {
-		return new User(user.get("first_name"), user.get("last_name"), user.get("email"), user.get("avatar"));
-	}
-	
-	@DataTableType
-	public UserCreateRequest userCreateRequest(Map<String, String> userCreate) {
-		UserCreateRequest.setUserCreateRequest(new UserCreateRequest(userCreate.get("name"), userCreate.get("job")));
-		return UserCreateRequest.getUserCreateRequest();
-	}
-	
-	@DataTableType
-	public UserUpdateRequest userUpdateRequest(Map<String, String> userUpdate) {
-		UserUpdateRequest.setUserUpdateRequest(new UserUpdateRequest(userUpdate.get("name"), userUpdate.get("job")));
-		return UserUpdateRequest.getUserUpdateRequest();
-	}
 
 }
